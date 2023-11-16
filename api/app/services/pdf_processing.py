0dcd13715ec6a0c7aacb9c672c81a822a3cf96c5
Giocrisrai Godoy <giocrisrai@gmail.com>
2023-11-15 23:09:48 -0300

    Add: pdf_processing create

--- api/app/services/pdf_processing.py
+++ api/app/services/pdf_processing.py
@@ -1,8 +1,7 @@
 import logging
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyMuPDFLoader
 from langchain.schema.document import Document
 from typing import Tuple, List
-import PyPDF2
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -10,30 +9,37 @@ logging.basicConfig(level=logging.INFO)
 
 def process_pdf(file_path: str) -> Tuple[List[Document], str]:
     """
-    Procesa un archivo PDF desde una ruta de archivo local para extraer texto y devuelve los datos extraídos.
+    Processes a PDF file, extracting text and metadata from each page.
+
+    This function uses PyMuPDFLoader to load the PDF file and extract the content of each page,
+    along with detailed metadata such as page number, title, author, etc.
+    The progress of the processing and any errors encountered are logged.
 
     Parameters:
-    - file_path (str): La ruta del archivo PDF a procesar.
+    - file_path (str): The path to the PDF file to be processed.
 
     Returns:
-    - Tuple[List[Document], str]: Una tupla que contiene los datos de texto extraídos y la ruta del archivo PDF.
+    - Tuple[List[Document], str]: A tuple containing a list of Document objects,
+      where each object represents a page of the PDF with its content and metadata,
+      and the path of the processed PDF file.
+
+    Raises:
+    - Exception: Propagates any exceptions that occur during the processing of the PDF.
     """
+
     try:
-        logging.info(f"Processing PDF: {file_path}")
+        logging.info(f"Starting PDF processing: {file_path}")
 
-        # Obtiene el número de páginas
-        with open(file_path, 'rb') as f:
-            pdf = PyPDF2.PdfReader(f)
-            numero_paginas = len(pdf.pages)
-        logging.info(f"Number of pages: {numero_paginas}")
+        # Load and process the PDF
+        loader = PyMuPDFLoader(file_path)
+        documents = loader.load()
 
-        # Procesa el PDF
-        loader = PyPDFLoader(file_path)
-        data = loader.load()
+        # Log the number of processed pages
+        number_of_pages = len(documents)
+        logging.info(f"Number of pages processed: {number_of_pages}")
 
-        logging.info(f"PDF processed successfully: {file_path}")
-
-        return data, file_path
+        logging.info(f"PDF processing completed successfully: {file_path}")
+        return documents, file_path
 
     except Exception as e:
         logging.error(f"Error processing the PDF: {e}")
