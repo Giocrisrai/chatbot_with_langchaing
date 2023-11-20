8be4eca8b2a8f622eaca6aafb034f7c3b5c22f3b
Giocrisrai Godoy <giocrisrai@gmail.com>
2023-11-19 23:02:21 -0300

    add: update chorma services

--- api/app/services/chroma_service.py
+++ api/app/services/chroma_service.py
@@ -1,50 +1,30 @@
+import logging
 from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
 import os
 
 
-def initialize_chroma(persist_directory: str):
+def get_chroma_db(embeddings, documents, path, recreate_chroma_db=False):
     """
-    Inicializa la base de datos Chroma con OpenAI Embeddings.
+    Create or load a Chroma vector store from documents and embeddings.
 
-    Args:
-    - persist_directory (str): Directorio para almacenar la base de datos de Chroma.
+    Parameters:
+    - embeddings: The embeddings to use for creating the Chroma vector store.
+    - documents: The documents to include in the Chroma vector store.
+    - path: The path where the Chroma vector store will be saved or loaded from.
+    - recreate_chroma_db (bool): If True, recreate the Chroma vector store; if False, load an existing one.
 
     Returns:
-    - Chroma: Instancia de la base de datos Chroma.
+    - Chroma: The Chroma vector store.
     """
-    os.environ['OPENAI_API_KEY'] = os.getenv(
-        'OPENAI_API_KEY', 'default_api_key')
-    embeddings = OpenAIEmbeddings()
-    return Chroma(persist_directory=persist_directory, embedding_function=embeddings)
-
-
-def store_document_in_chroma(chroma_db, document_content, unique_filename):
-    """
-    Convierte el contenido del documento en embedding y lo almacena en Chroma.
-
-    Args:
-    - chroma_db: Instancia de la base de datos Chroma.
-    - document_content (str): Contenido del documento.
-    - unique_filename (str): Nombre único del archivo/documento.
-
-    Returns:
-    - str: Mensaje de confirmación.
-    """
-    embedding = chroma_db.embedding_function.embed_text(document_content)
-    chroma_db.add_documents([(unique_filename, embedding)])
-    return "Documento almacenado en Chroma."
-
-
-def search_similar_documents(chroma_db, query):
-    """
-    Realiza una búsqueda de similitud en la base de datos Chroma.
-
-    Args:
-    - chroma_db: Instancia de la base de datos Chroma.
-    - query (str): Consulta para la búsqueda de similitud.
-
-    Returns:
-    - List[Document]: Lista de documentos similares encontrados.
-    """
-    return chroma_db.similarity_search(query)
+    try:
+        if recreate_chroma_db:
+            logging.info("RECREATING CHROMA DB")
+            return Chroma.from_documents(
+                documents=documents, embedding=embeddings, persist_directory=path
+            )
+        else:
+            logging.info("LOADING EXISTING CHROMA")
+            return Chroma(persist_directory=path, embedding_function=embeddings)
+    except Exception as e:
+        logging.error(f"Error in get_chroma_db: {e}")
+        raise e
