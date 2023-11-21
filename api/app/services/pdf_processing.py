52f78b98559a3419e384a2b85cd0260211315b65
Giocrisrai Godoy <giocrisrai@gmail.com>
2023-11-20 23:07:45 -0300

    Add: change how to process pdf

--- api/app/services/pdf_processing.py
+++ api/app/services/pdf_processing.py
@@ -9,35 +9,25 @@ logging.basicConfig(level=logging.INFO)
 
 def process_pdf(file_path: str) -> List[Document]:
     """
-    Processes a PDF file, extracting text and metadata from each page.
-
-    This function uses PyMuPDFLoader to load the PDF file and extract the content of each page,
-    along with detailed metadata such as page number, title, author, etc.
-    The progress of the processing and any errors encountered are logged.
+    Processes a PDF file from a local file path.
 
     Parameters:
-    - file_path (str): The path to the PDF file to be processed.
+    - file_path (str): The path to the PDF file.
 
     Returns:
-    - List[Document]: A list of Document objects,
-      where each object represents a page of the PDF with its content and metadata.
-
-    Raises:
-    - Exception: Propagates any exceptions that occur during the processing of the PDF.
+    - List[Document]: A list of Document objects.
     """
-
     try:
-        logging.info(f"Starting PDF processing: {file_path}")
+        logging.info(f"Starting PDF processing for file: {file_path}")
 
         # Load and process the PDF
         loader = PyMuPDFLoader(file_path)
         documents = loader.load()
 
-        # Log the number of processed pages
         number_of_pages = len(documents)
         logging.info(f"Number of pages processed: {number_of_pages}")
 
-        logging.info(f"PDF processing completed successfully: {file_path}")
+        logging.info("PDF processing completed successfully")
         return documents
 
     except Exception as e:
