50c7b481d701b7afefa7a66f5703ff27f1930718
Giocrisrai Godoy <giocrisrai@gmail.com>
2023-11-20 23:07:24 -0300

    Add: modify data how to create embeddings

--- api/app/services/file_postprocessing.py
+++ api/app/services/file_postprocessing.py
@@ -1,6 +1,5 @@
 import os
 from typing import List
-
 from langchain.schema.document import Document
 from langchain.embeddings import OpenAIEmbeddings, SentenceTransformerEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -12,60 +11,59 @@ load_dotenv()
 
 def split_data(data: List[Document]) -> List[Document]:
     """
-    Split the data into smaller chunks using RecursiveCharacterTextSplitter.
+    Splits a list of Document objects into smaller chunks.
+
+    This function uses a RecursiveCharacterTextSplitter to split each document's text into smaller chunks. 
+    This is useful for processing long documents in systems with constraints on input size.
 
     Parameters:
-    data (List[Document]): A list of Document objects containing the text data and metadata.
+    - data (List[Document]): A list of Document objects to be split.
 
     Returns:
-    List[Document]: A list of Document objects containing the text data split into smaller chunks.
+    - List[Document]: A list of Document objects, each containing a portion of the original text.
     """
     # Configure chunk size and overlap
-    chunk_size = 1000
-    chunk_overlap = 200
+    chunk_size = 1000  # The size of each chunk
+    chunk_overlap = 200  # The overlap between chunks
 
     # Create a TextSplitter object
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap
-    )
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap)
 
     # Split documents into smaller chunks
     return text_splitter.split_documents(data)
 
 
-def create_embeddings_openai(data: List[Document]) -> List[Document]:
+def create_embeddings_openai() -> OpenAIEmbeddings:
     """
-    Create embeddings for the data using OpenAIEmbeddings.
+    Creates an embeddings object using OpenAI's GPT-3 model.
 
-    Parameters:
-    data (List[Document]): A list of Document objects containing the text data and metadata.
+    This function initializes an embeddings object for generating embeddings using OpenAI's API. 
+    It requires an API key, which should be set in the environment variables.
 
     Returns:
-    List[Document]: A list of Document objects containing the text data, metadata, and embeddings.
+    - OpenAIEmbeddings: An instance of OpenAIEmbeddings for generating text embeddings.
     """
     # Set up the OpenAI API key from environment variables
     openai_api_key = os.getenv('OPENAI_API_KEY', 'default_api_key')
     os.environ['OPENAI_API_KEY'] = openai_api_key
 
     # Create an OpenAIEmbeddings object
-    embeddings = OpenAIEmbeddings()
-
-    # Generate text embeddings for the documents
-    return embeddings.embed_documents(data)
+    return OpenAIEmbeddings()
 
 
-def create_embeddings_open_source(model_name: str) -> List[Document]:
+def create_embeddings_open_source(model_name: str) -> SentenceTransformerEmbeddings:
     """
-    Create embeddings for the data using SentenceTransformerEmbeddings.
+    Creates an embeddings object using the Sentence Transformer model.
+
+    This function initializes an embeddings object using a specified Sentence Transformer model.
+    The model name should correspond to one of the pre-trained models available in the Sentence Transformers library.
 
     Parameters:
-    model_name (str): The name of the model to use for generating embeddings.
+    - model_name (str): The name of the Sentence Transformer model to be used.
 
     Returns:
-    List[Document]: A list of Document objects containing the text data, metadata, and embeddings.
+    - SentenceTransformerEmbeddings: An instance of SentenceTransformerEmbeddings for generating text embeddings.
     """
     # Create a SentenceTransformerEmbeddings object
-    embeddings = SentenceTransformerEmbeddings(model_name=model_name)
-
-    # Generate text embeddings for the documents
-    return embeddings.embed_documents(data)
+    return SentenceTransformerEmbeddings(model_name=model_name)
