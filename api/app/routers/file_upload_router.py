dc2aa8dd421a986768b8f310903940e92c0a3830
Giocrisrai Godoy <giocrisrai@gmail.com>
2023-11-20 23:06:26 -0300

    Add: upload file route change how manipulate data

--- api/app/routers/file_upload_router.py
+++ api/app/routers/file_upload_router.py
@@ -1,69 +1,71 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, UploadFile, File
 from typing import List, Dict, Union
 import logging
+import os
 from app.services.pdf_processing import process_pdf
 from app.services.docx_processing import process_docx
 from app.services.pptx_processing import process_pptx
 from app.services.xlsx_processing import process_xlsx
 from app.services.csv_processing import process_csv
-from app.services.file_postprocessing import split_data, create_embeddings_openai, create_embeddings_open_source
+from app.services.file_postprocessing import split_data, create_embeddings_open_source
 from app.services.chroma_service import get_chroma_db
 
 router = APIRouter()
 
 
 @router.post("/multipleupload/")
-async def multiple_upload_route(files: List[UploadFile] = File(...)) -> Dict[str, Union[str, bool]]:
-    """
-    Process and store multiple uploaded files, storing their vector representations in Chroma.
-
-    Args:
-    - files (List[UploadFile]): List of files to be uploaded.
-
-    Returns:
-    - Dict[str, Union[str, bool]]: A dictionary with the results of processing and storing each file.
-    """
+async def multiple_upload_route(files: List[UploadFile] = File(...)) -> Dict[str, List[Dict[str, Union[str, bool]]]]:
     results = []
+
+    if not files:
+        return {"results": results}
+
+    temp_dir = "data/raw"  # Directorio temporal para guardar los archivos
+    os.makedirs(temp_dir, exist_ok=True)  # Crear el directorio si no existe
+
     for file in files:
         unique_filename = file.filename
-        file_content = await file.read()
         file_extension = unique_filename.split(".")[-1].lower()
+        file_path = os.path.join(temp_dir, unique_filename)
 
-        if file_extension not in ["pdf", "docx", "pptx", "mp3", "m4a", "csv"]:
-            logging.error(f"Unsupported file extension: {file_extension}")
-            results.append({"filename": unique_filename, "status": "Failed",
-                           "message": "Unsupported file extension"})
+        supported_extensions = ["pdf", "docx", "pptx", "xlsx", "csv"]
+        if file_extension not in supported_extensions:
+            error_message = f"Unsupported file extension: {file_extension}"
+            logging.error(error_message)
+            results.append({"filename": unique_filename,
+                            "status": False, "message": error_message})
             continue
 
         try:
-            # Process files based on their type
+            with open(file_path, "wb") as out_file:
+                out_file.write(await file.read())
+
             if file_extension == "pdf":
-                data = process_pdf(file_content, unique_filename)
+                data = process_pdf(file_path)
             elif file_extension == "docx":
-                data = process_docx(file_content, unique_filename)
+                data = process_docx(file_path)
             elif file_extension == "pptx":
-                data = process_pptx(file_content, unique_filename)
+                data = process_pptx(file_path)
             elif file_extension == "xlsx":
-                data = process_xlsx(file_content, unique_filename)
+                data = process_xlsx(file_path)
             elif file_extension == "csv":
-                data = process_csv(file_content, unique_filename)
+                data = process_csv(file_path)
 
-            # Split the data into smaller chunks
             documents = split_data(data)
-
-            # Create embeddings
             model_name = "all-MiniLM-L6-v2"
             embeddings = create_embeddings_open_source(model_name=model_name)
-
-            # Store in Chroma
             vectorstore_chroma = get_chroma_db(
                 embeddings, documents, "chroma_docs", recreate_chroma_db=False)
 
-            results.append({"filename": unique_filename, "status": "Success",
-                           "message": "File processed and stored in Chroma"})
+            results.append({"filename": unique_filename, "status": True,
+                            "message": "File processed and stored successfully"})
         except Exception as e:
-            logging.exception(f"An error occurred: {str(e)}")
-            results.append({"filename": unique_filename, "status": "Failed",
-                           "message": "An internal server error occurred"})
+            error_message = f"An error occurred: {str(e)}"
+            logging.exception(error_message)
+            results.append({"filename": unique_filename,
+                            "status": False, "message": error_message})
+        finally:
+            if os.path.exists(file_path):
+                os.remove(file_path)
 
     return {"results": results}
